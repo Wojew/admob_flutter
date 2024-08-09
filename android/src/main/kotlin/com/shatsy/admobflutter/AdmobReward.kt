7bc120d92666461852012b589aedceba91eeb533
Wojciech Zahradnik <wojciechzahradnik@gmail.com>
2024-08-09 08:54:29 +0200

    Update AdmobReward.kt

--- android/src/main/kotlin/com/shatsy/admobflutter/AdmobReward.kt
+++ android/src/main/kotlin/com/shatsy/admobflutter/AdmobReward.kt
@@ -59,10 +59,8 @@ class AdmobReward(private val flutterPluginBinding: FlutterPlugin.FlutterPluginB
                                 allAds[id]?.adListener?.onAdClosed()
                             }
 
-                            override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
-                                adError?.let {
-                                    allAds[id]?.adListener?.onAdFailedToLoad(LoadAdError(it.code, it.message, "admob_flutter", adError, null))
-                                }
+                            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                                allAds[id]?.adListener?.onAdFailedToLoad(LoadAdError(adError.code, adError.message, "admob_flutter", adError, null))
                             }
 
                             override fun onAdShowedFullScreenContent() {
